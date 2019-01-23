fd6a352b95454e437aeb1384ec83c186b4cdd1da
waqashamid <waqashamid722@gmail.com>
2019-01-23 14:05:29 +0530

    Added urls to the search API

---
+++ papers/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from .views import *
+
+urlpatterns = [
+    url(
+        r'^search/$',
+        FetchPapersView.as_view(),
+        name='search-papers',
+    ),
+]
