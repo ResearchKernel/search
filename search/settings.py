217e44a6f47846df314cfb381ade33de1aa3e2c9
waqashamid <waqashamid722@gmail.com>
2019-02-25 17:06:25 +0530

    Rectified formats

--- search/settings.py
+++ search/settings.py
@@ -66,7 +66,9 @@ ROOT_URLCONF = 'search.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
