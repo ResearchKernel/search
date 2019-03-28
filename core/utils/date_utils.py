502ed60b40c0232c0bfea67a4082bc0871424623
waqashamid <waqashamid722@gmail.com>
2019-03-28 15:13:53 +0530

    Added core_utils

---
+++ core/utils/date_utils.py
@@ -0,0 +1,13 @@
+import datetime
+
+
+def convert_date_to_es_format(date):
+    try:
+        datetime.datetime.strptime(date, '%d/%m/%Y')
+    except ValueError:
+        raise ValueError("Incorrect data format, should be DD-MM-YYYY")
+
+    date_list = date.split('/')
+    new_date = date_list[2] + '-' + date_list[1] + '-' + date_list[0]
+
+    return new_date
