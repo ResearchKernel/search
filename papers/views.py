a5f396a56dfa1c7fde4082dea81861ac6233c263
waqashamid <waqashamid722@gmail.com>
2019-03-29 15:25:18 +0530

    Added new api for recent papers

--- papers/views.py
+++ papers/views.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from core.utils.date_utils import convert_date_to_es_format
+from core.utils.date_utils import convert_date_to_es_format, get_today_date
 from search import settings
 
 from .constants import SEARCH_URL
@@ -15,22 +15,57 @@ class FetchPapersView(APIView):
     def get(self, request, **kwargs):
 
         # Fetching query params and preparing request data
-        start_date = request.GET.get('start_date')
-        end_date = request.GET.get('end_date')
-        start_date = convert_date_to_es_format(start_date)
-        end_date = convert_date_to_es_format(end_date)
-        query = {
-            "query": {
-                "range": {
-                    "created": {
-                    "gte": start_date,
-                    "lte": end_date,
-                    "format": "yyyy-MM-dd"
+        start_date = request.GET.get('start_date', None)
+        end_date = request.GET.get('end_date', None)
+        if start_date:
+            start_date = convert_date_to_es_format(start_date)
+        if end_date:
+            end_date = convert_date_to_es_format(end_date)
+
+        if not end_date:
+            query = {
+                "query": {
+                    "match" : {
+                        "created" : start_date
                     }
                 }
             }
-        }
+        else:
+            query = {
+                "query": {
+                    "range": {
+                        "created": {
+                        "gte": start_date,
+                        "lte": end_date,
+                        "format": "yyyy-MM-dd"
+                        }
+                    }
+                }
+            }
 
+        # Initializing ElasticSearch client
+        es = Elasticsearch()
+        # Sending out request to ElasticSearch server to return search results
+        response = es.search(
+            index=settings.ES_INDEX_NAME,
+            body=query
+        )
+
+        return Response(response, status=status.HTTP_200_OK)
+
+
+class RecentPaperView(APIView):
+
+    def get(self, request, **kwargs):
+
+        today = get_today_date()
+        query = {
+            "query": {
+                "match": {
+                    "created": today
+                }
+            }
+        }
         # Initializing ElasticSearch client
         es = Elasticsearch()
         # Sending out request to ElasticSearch server to return search results
