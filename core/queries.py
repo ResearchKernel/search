4d28bf8987454cdaed1497dbc0c4ffb229cb96e7
Prakritidev Verma <prakritidevverma@gmail.com>
2019-08-09 01:32:36 +0530

    added pagination in es APIs

--- core/queries.py
+++ core/queries.py
@@ -2,59 +2,62 @@
 # A reservoir to maintain the various queries used in the search APIs
 
 abstract_primary_category_query = {
-  "query": {
-    "bool": {
-      "must": [
-        {
-          "match": {
-            "primary_category": None,
-          },
+    "from": 0, "size": 10,
+    "query": {
+        "bool": {
+            "must": [
+                {
+                    "match": {
+                        "primary_category": None,
+                    },
+                },
+            ],
+            "filter": [
+                {
+                    "range": {
+                        "created": {
+                            "gte": None,
+                            "lte": None,
+                            "format": "yyyy-MM-dd",
+                        },
+                    },
+                },
+            ],
         },
-      ],
-      "filter": [
-        {
-          "range": {
-            "created": {
-              "gte": None,
-              "lte": None,
-              "format": "yyyy-MM-dd",
-            },
-          },
-        },
-      ],
     },
-  },
 }
 
 abstract_category_query = {
-  "query": {
-    "bool": {
-      "must": [
-        {
-          "match": {
-            "categories": None,
-          },
+    "from": 0, "size": 10,
+    "query": {
+        "bool": {
+            "must": [
+                {
+                    "match": {
+                        "categories": None,
+                    },
+                },
+            ],
+            "filter": [
+                {
+                    "range": {
+                        "created": {
+                            "gte": None,
+                            "lte": None,
+                            "format": "yyyy-MM-dd",
+                        },
+                    },
+                },
+            ],
         },
-      ],
-      "filter": [
-        {
-          "range": {
-            "created": {
-              "gte": None,
-              "lte": None,
-              "format": "yyyy-MM-dd",
-            },
-          },
-        },
-      ],
     },
-  },
 }
 
 abstract_recent_query = {
-  "query": {
-    "match": {
-      "created": None,
+    "from": 0, "size": 10,
+    "query": {
+        "match": {
+            "created": None,
+        }
     }
-  }
 }
