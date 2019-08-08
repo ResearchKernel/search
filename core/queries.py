50758096da0c938d7ccd6084d595083ce0004523
Prakritidev Verma <prakritidevverma@gmail.com>
2019-08-09 01:49:06 +0530

    Added Search APIs with Pagination

--- core/queries.py
+++ core/queries.py
@@ -61,3 +61,166 @@ abstract_recent_query = {
         }
     }
 }
+
+#----------------------------------------Universal Search APIs----------------------------------------#
+
+universal_search = {
+    "from": 0, "size": 10,
+    "query": {
+        "multi_match": {
+            "query": None,
+            "type": "cross_fields",
+            "fields": [
+                "arxiv_id",
+                "abstract",
+                "title",
+                "abstract",
+                "authors",
+                "doi"
+            ]
+        }
+    }
+}
+
+primary_category_search = {
+    "from": 0, "size": 10,
+    "query": {
+        "bool": {
+            "should": [
+                {
+                    "multi_match": {
+                        "query": None,
+                        "type": "cross_fields",
+                        "fields": [
+                            "arxiv_id",
+                            "abstract",
+                            "title",
+                            "abstract",
+                            "authors",
+                            "doi"
+                        ]
+                    }
+                }
+            ],
+            "filter": {
+                "term": {
+                    "primary_category": None
+                }
+            }
+        }
+    }
+}
+
+#----------------------------------------Advanced Search APIs------------------------------------------#
+# Search for authors
+
+search_author = {
+    "from": 0, "size": 10,
+    "query": {
+        "bool": {
+            "must": [
+                {
+                    "match": {
+                        "authors": None
+                    }
+                }
+            ],
+            "filter": [
+                {
+                    "range": {
+                        "created": {
+                            "gte": None,
+                            "lte": None,
+                            "format": "yyyy-MM-dd"
+                        }
+                    }
+                }
+            ]
+        }
+    }
+}
+
+# Search for Title
+
+search_title = {
+    "from": 0, "size": 10,
+    "query": {
+        "bool": {
+            "must": [
+                {
+                    "match": {
+                        "title": None
+                    }
+                }
+            ],
+            "filter": [
+                {
+                    "range": {
+                        "created": {
+                            "gte": None,
+                            "lte": None,
+                            "format": "yyyy-MM-dd"
+                        }
+                    }
+                }
+            ]
+        }
+    }
+}
+
+# Search for Abstract
+
+search_abstract = {
+    "from": 0, "size": 10,
+    "query": {
+        "bool": {
+            "must": [
+                {
+                    "match": {
+                        "abstract": None
+                    }
+                }
+            ],
+            "filter": [
+                {
+                    "range": {
+                        "created": {
+                            "gte": None,
+                            "lte": None,
+                            "format": "yyyy-MM-dd"
+                        }
+                    }
+                }
+            ]
+        }
+    }
+}
+
+
+# Search for Arxiv_id
+
+search_arxiv = {
+    "from": 0, "size": 10,
+    "query": {
+        "bool": {
+            "must": [
+                {
+                    "match": {
+                        "arxiv_id": None
+                    }
+                }
+            ],
+            "filter": [
+                {
+                    "range": {
+                        "created": {
+                            "gte": None,
+                            "lte": None,
+                            "format": "yyyy-MM-dd"
+                        }
+                    }
+                }
+            ]
+        }
+    }
+}
